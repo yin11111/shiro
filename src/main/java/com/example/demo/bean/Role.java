66524ed26d1ad2f7390907cfc75ec5ab21c5601a
tk <yinhaipeng1991@163.com>
2019-11-08 16:53:24 +0800

    springBoot整合shiro

---
+++ src/main/java/com/example/demo/bean/Role.java
@@ -0,0 +1,45 @@
+package com.example.demo.bean;
+
+import java.util.Set;
+
+public class Role {
+    private String id;
+    private String roleName;
+
+    public Role(String id, String roleName, Set<Permissions> permissions) {
+        this.id = id;
+        this.roleName = roleName;
+        this.permissions = permissions;
+    }
+
+    /**
+     * 角色对应权限集合
+     */
+
+
+    private Set<Permissions> permissions;
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
+
+    public Set<Permissions> getPermissions() {
+        return permissions;
+    }
+
+    public void setPermissions(Set<Permissions> permissions) {
+        this.permissions = permissions;
+    }
+}
---
+++ src/main/java/com/example/demo/service/UserService.java
@@ -0,0 +1,7 @@
+package com.example.demo.service;
+
+import com.example.demo.bean.User;
+
+public interface UserService {
+    User getUserByName(String name);
+}
